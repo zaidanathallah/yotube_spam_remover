8d5f837a8d1e1ee217379cabf3334b3372f79cb6
zai <114717471+zaidanathallah@users.noreply.github.com>
2025-04-22 12:44:56 +0700

    Update judol_patterns.py

--- judol_patterns.py
+++ judol_patterns.py
@@ -1,12 +1,11 @@
-
 import re
 
 def is_judol_comment(comment):
     patterns = [
-        r"🚩.*🚩",
-        r"([A-Z0-9]\s*){5,}",
-        r"(\w+)\1{2,}",
-        r"x\s*[0-9]+",
-        r"weton|togel|slot|bo"
+        r"🚩.*🚩",                      # Emoji bendera
+        r"([A-Z0-9]\s*){5,}",          # Huruf kapital berlebihan
+        r"(\w+)\1{2,}",                # Kata berulang
+        r"x\s*[0-9]+",                 # Format x 500
+        r"weton|togel|slot|bo"        # Kata kunci spam
     ]
     return any(re.search(p, comment, re.IGNORECASE) for p in patterns)
